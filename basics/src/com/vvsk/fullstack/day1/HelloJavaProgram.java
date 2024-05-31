4123255414c03592ba0e4119bd52875b070765f9
vvskchaitanya <vvskchaitanya@gmail.com>
2024-05-31 04:40:49 +0000

    initial commit

---
+++ basics/src/com/vvsk/fullstack/day1/HelloJavaProgram.java
@@ -0,0 +1,12 @@
+package com.vvsk.fullstack.day1;
+
+/** Example Program to demonstrate the basic structure of Java Program */
+public class HelloJavaProgram {
+
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Hello All, This is my first Java Program!");
+        
+    }
+
+}
---
+++ basics/src/com/vvsk/fullstack/day1/JavaExampleProgram.java
@@ -0,0 +1,14 @@
+package com.vvsk.fullstack.day1;
+
+/** Example Program to demonstrate the basic structure of Java Program */
+public class JavaExampleProgram {
+
+    public static void main(String[] args) {
+		
+		int x = 10;
+		
+		System.out.println("Value of X : "+x);
+
+	}
+
+}
